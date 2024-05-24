1d997c72c1c9ffd6a6442dde7d11eef1f21e2d4e
anurag-stpl <anurag.d@srmtechsol.com>
2024-05-24 11:17:23 +0530

    update

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const http=require('http');
 const fs=require('fs');
 var requests=require("requests");
 const homeFile=fs.readFileSync("home.html","utf-8");
-const replaceval=(tempVal,OrgVal)=>{
-    let temperature=tempVal.replace("{%tempval%}",OrgVal.main.temp);
-     temperature=temperature.replace("{%tempmin%}",OrgVal.main.temp_min);
-     temperature=temperature.replace("{%tempmax%}",OrgVal.main.temp_max);
-     temperature=temperature.replace("{%location%}",OrgVal.name);
-     temperature=temperature.replace("{%country%}",OrgVal.sys.country);
-     temperature=temperature.replace("{%tempstatus%}",OrgVal.weather[0].main);
-
-     return temperature;
+const replaceval = (tempVal, OrgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", Math.round(OrgVal.main.temp - 273.15));
+    temperature = temperature.replace("{%tempmin%}", Math.round(OrgVal.main.temp_min - 273.15));
+    temperature = temperature.replace("{%tempmax%}", Math.round(OrgVal.main.temp_max - 273.15));
+    temperature = temperature.replace("{%location%}", OrgVal.name);
+    temperature = temperature.replace("{%country%}", OrgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", OrgVal.weather[0].main);
+    return temperature;
 }
+
+
 const server=http.createServer((req,res)=>{
     if(req.url="/"){
         requests("https://api.openweathermap.org/data/2.5/weather?q=Lucknow&appid=1e892a7a6f3099294a24bfcdc08f2c4b")
@@ -22,6 +23,7 @@ const server=http.createServer((req,res)=>{
             const realTimeData=arrData.map((val)=>replaceval(homeFile,val)).join("");
             res.write(realTimeData);
             // console.log(realTimeData);
+       
             
         })
         .on("end",(err)=>{
